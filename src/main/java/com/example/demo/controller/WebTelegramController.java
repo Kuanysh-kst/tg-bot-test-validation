4934aa95d48696c41cb0cf6b27604980fb131280
Kyanysh <kuanysh4646@gmail.com>
2025-05-27 23:51:05 +0500

    added readme file
    Signed-off-by: Kyanysh <kuanysh4646@gmail.com>

--- src/main/java/com/example/demo/controller/WebTelegramController.java
+++ src/main/java/com/example/demo/controller/WebTelegramController.java
@@ -8,7 +8,11 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
